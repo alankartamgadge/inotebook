429ed00ff6643d25e2e2f621374dd3066acc8ffe
Alankar Tamgadge <alankar_tamgadge@vistasoftwares.com>
2023-03-27 09:40:49 +0530

    Added validations

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,26 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req, res) => {
-
-    const user = User(req.body);
-    user.save();
-
-    console.log(req.body);
-    res.send(req.body);
-})
+router.get(
+  "/",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("password", "Enter a valid password").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    }).then((user) => res.json(user)).catch(err=>console.log(err));
+  }
+);
 
 module.exports = router;
