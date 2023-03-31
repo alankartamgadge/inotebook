959c2b7687cd5cfdd8a288d50a73f847994cf1e2
Alankar Tamgadge <alankar_tamgadge@vistasoftwares.com>
2023-03-31 19:04:45 +0530

    endpoint to log in via secured pass added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,11 +2,11 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcryptjs')
-var jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+const { findOne } = require("../models/User");
 
-const JWT_SECRET= "boy"
-
+const JWT_SECRET = "boy";
 //(/api/auth/createuser)
 
 router.post(
@@ -14,7 +14,7 @@ router.post(
   [
     body("email", "Enter a valid email").isEmail(),
     body("name", "Enter a valid name").isLength({ min: 3 }),
-    body("password", "Enter a valid password").isLength({ min: 5 }),
+    body("password", "Enter a valid password").isLength({ min: 3 }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -39,19 +39,61 @@ router.post(
       });
 
       const data = {
-        user:{
-          id:user.id
-        }
-      }
+        user: {
+          id: user.id,
+        },
+      };
 
-      const authToken =jwt.sign(data, JWT_SECRET);
-      console.log(authToken)
+      const authToken = jwt.sign(data, JWT_SECRET);
 
-      res.json(user);
-      
+      res.json(authToken);
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some Error Occured!");
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Enter a valid password").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (!user) {
+        return res.status(400).json({ error: "Invalid credentials!" });
+      }
+
+      let checkPass = await bcrypt.compare(password, user.password);
+
+
+      if (!checkPass) {
+        return res.status(400).json({ error: "Invalid credentials!" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+
+      res.json(authToken);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
     }
   }
 );
